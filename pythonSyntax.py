fbb183cb423aae5ccbbcaeefabf1bc879a2bd12f
Jhoselyn Carballo Sanchez <jhoselyncarballosanchez@gmail.com>
2022-02-01 06:11:06 -0600

    add clase2

---
+++ pythonSyntax.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 06:08:24 2022
+
+@author: JHOSS
+"""
+
+NAME = 'jhoselyn'
+FULL NAME = 'jhoselyn carballo sanchez'
+print (NAME)
+print (FULL_NAME)
+# TIPOS DE DATOS
+entero = 90
+print (f' el entero tiene valor de {entero}')
+decimal = 6.14
+print (f' el valor del decimal es {decimal}')
+caracter = 'J'
+print (f' el valor del decimal es {caracter}')
+cadena = 'primera cadena'
+print (f' el valor del decimal es {cadena}')
+cadena_2 = "segunda cadena"
+print (f' el valor del decimal es {cadena_2}')
+booleano = False # valores True o False
+print (f' el valor del decimal es {booleano}')
+# LISTA
+list=[1, 2, 3, 4]
+print (f' el valor de la lista es : {list}')
